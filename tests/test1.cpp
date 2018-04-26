37f95b932c4fac98b6579bbb60107bdfe6157c0b
amirnyy <37066891+amirnyy@users.noreply.github.com>
2018-04-26 11:55:38 +0300

    Update test1.cpp

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -7,7 +7,7 @@ REQUIRE(m6.empty() == true);
 }
 TEST_CASE("Show Matrix1") {
     Matrix <int, 2, 1> m1{3,5};
-    std::string text = "2 1\n3\n5"
+    std::string text = "2 1\n3\n5";
     std::ofstream fout("Matrix.bin");
     fout << m1;
     fout.close();
@@ -35,7 +35,7 @@ TEST_CASE("Matrix comparison") {
 }
 TEST_CASE("Show Matrix2") {
     Matrix <int, 3, 2> m2{1,2,3,4,5,6};
-    std::string text = "3 2\n1 2\n3 4\n5 6"
+    std::string text = "3 2\n1 2\n3 4\n5 6";
     std::ofstream fout("Matrix.bin");
     fout << m2;
     fout.close();
@@ -49,7 +49,7 @@ TEST_CASE("Show Matrix2") {
 TEST_CASE("Show Matrix3") {
     Matrix <int, 3, 2> m2{1, 2, 3, 4, 5, 6};
     Matrix <int, 3, 2> m3{m2};
-    std::string text = "3 2\n1 2\n3 4\n5 6"
+    std::string text = "3 2\n1 2\n3 4\n5 6";
     std::ofstream fout("Matrix.bin");
     fout << m3;
     fout.close();
@@ -64,7 +64,7 @@ TEST_CASE("Sum Matrix") {
     Matrix <int, 3, 2> m2{1, 2, 3, 4, 5, 6};
     Matrix <int, 3, 2> m3{m2};
     Matrix <int, 3, 2> m5{0, 2, 4, 6, 8, 10};
-    std::string text = "3 2\n1 4\n7 10\n13 16"
+    std::string text = "3 2\n1 4\n7 10\n13 16";
     std::ofstream fout("Matrix.bin");
     fout << m3 + m5;
     fout.close();
@@ -79,7 +79,7 @@ TEST_CASE("Diff Matrix") {
     Matrix <int, 3, 2> m2{1, 2, 3, 4, 5, 6};
     Matrix <int, 3, 2> m3{m2};
     Matrix <int, 3, 2> m5{0, 2, 4, 6, 8, 10};
-    std::string text = "3 2\n1 2\n3 4\n5 6"
+    std::string text = "3 2\n1 2\n3 4\n5 6";
     std::ofstream fout("Matrix.bin");
     fout << m3 - m5;
     fout.close();
@@ -95,7 +95,7 @@ TEST_CASE("Swap Matrix") {
     Matrix <int, 3, 2> m3{m2};
     Matrix <int, 3, 2> m6{m3};
     m6[0][0]=15;
-    std::string text = "3 2\n15 2\n1 2\n3 4"
+    std::string text = "3 2\n15 2\n1 2\n3 4";
     std::ofstream fout("Matrix.bin");
     fout << m3.swap_(m6);
     fout.close();
